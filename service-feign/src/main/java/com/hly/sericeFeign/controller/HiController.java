b93c419c0d396b7e91c07b4e50ffb72cc0498ebf
黄良运 <1136513099@qq.com>
2018-10-16 21:26:45 +0800

    服务注册与发现eureka，负载均衡ribbon,feign

--- service-feign/src/main/java/com/hly/sericeFeign/controller/HiController.java
+++ service-feign/src/main/java/com/hly/sericeFeign/controller/HiController.java
@@ -15,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HiController {
 
-
     //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
     @Autowired
     SchedualServiceHi schedualServiceHi;
 
+    /**
+     * 消费服务
+     * @param name
+     * @return
+     */
     @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name) {
         return schedualServiceHi.sayHiFromClientOne( name );
