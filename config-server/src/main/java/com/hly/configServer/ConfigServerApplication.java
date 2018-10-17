c5dbf31af8f896e6e8b8ab15f3cde831c6aa0cf0
黄良运 <1136513099@qq.com>
2018-10-17 23:21:37 +0800

    配置中心Config

--- config-server/src/main/java/com/hly/configServer/ConfigServerApplication.java
+++ config-server/src/main/java/com/hly/configServer/ConfigServerApplication.java
@@ -1,10 +1,9 @@
 package com.hly.configServer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
 @EnableConfigServer
