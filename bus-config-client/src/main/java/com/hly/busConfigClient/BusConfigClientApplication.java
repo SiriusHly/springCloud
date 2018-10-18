a061af7f386bd082a1ee6f561d9eeeea0e131594
黄良运 <1136513099@qq.com>
2018-10-18 16:45:43 +0800

    服务链路追踪Spring Cloud Sleuth

---
+++ bus-config-client/src/main/java/com/hly/busConfigClient/BusConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.hly.busConfigClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusConfigClientApplication.class, args);
+	}
+}
