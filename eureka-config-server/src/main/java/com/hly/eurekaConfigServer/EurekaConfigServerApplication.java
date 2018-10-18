36e219a033a289868b04833663bda492da5dca98
黄良运 <1136513099@qq.com>
2018-10-18 15:32:27 +0800

    分布式配置中心

--- eureka-config-server/src/main/java/com/hly/eurekaConfigServer/EurekaConfigServerApplication.java
+++ eureka-config-server/src/main/java/com/hly/eurekaConfigServer/EurekaConfigServerApplication.java
@@ -1,18 +1,16 @@
-package com.hly.configServer;
+package com.hly.eurekaConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableConfigServer
-@EnableEurekaClient
-public class ConfigServerApplication {
+public class EurekaConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(EurekaConfigServerApplication.class, args);
 	}
-
 }
